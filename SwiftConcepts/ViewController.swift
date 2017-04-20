1772ee1c313747f871d42d9abe789b910e331bf5
saikiran komirishetty <saikiran.komirishetty@gmail.com>
2017-04-20 15:08:32 +0530

    Initial commit

---
+++ SwiftConcepts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftConcepts
+//
+//  Created by Komirishetti, Saikiran (Ext) on 20/04/17.
+//  Copyright © 2017 Saikiran Komirishetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
